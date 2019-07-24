bf7e50e7c705fe4843c9e2667a209bfac16d14c7
DouglasKGlover <douglas.glover@outlook.com>
2019-07-24 18:20:34 -0400

    Updated landing page for temporary view (under construction)

--- pages/index.vue
+++ pages/index.vue
@@ -3,36 +3,16 @@
     <div>
       <logo />
       <h1 class="title">
-        douglasglover-ca
+        DouglasGlover.ca
       </h1>
       <h2 class="subtitle">
-        My portfolio site; IDE inspired version
+        Currently under construction (as of July 2019)
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
+<script></script>
 
 <style>
 .container {
