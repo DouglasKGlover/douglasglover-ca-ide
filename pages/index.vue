8db87e23fbf66806fd2781448be48a56fdb91f44
DouglasKGlover <douglas.glover@outlook.com>
2019-07-24 19:39:01 -0400

    added a form for people to get in touch

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,22 @@
       <h2 class="subtitle">
         Currently under construction (July 2019)
       </h2>
+
+      <p>In the meantime, feel free to reach out:</p>
+      <form action="POST" netlify>
+        <p>
+          <label>Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Email: <input type="email" name="email"/></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   </div>
 </template>
@@ -14,6 +30,10 @@
 <script></script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -43,6 +63,46 @@
 
 .links {
   padding-top: 15px;
+}
+
+form {
+  display: block;
+  margin: 10px auto;
+  max-width: 100%;
+  border: 1px solid lightblue;
+  padding: 10px;
+  text-align: left;
+  transition: 0.25s ease;
+}
+
+form:focus-within,
+form:hover {
+  box-shadow: -2px 2px 5px rgba(0, 0, 0, 0.25);
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+input {
+  height: 2em;
+}
+
+textarea {
+  height: 8em;
+}
+
+button {
+  display: block;
+  margin: 0 auto;
+  background: lightseagreen;
+  border: none;
+  color: white;
+  padding: 1em 3em;
+  font-weight: bold;
 }
 
 @media (max-width: 700px) {
