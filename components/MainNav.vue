6e556acb2e406f9d47dcac66a0a3e6fdcb85d620
DouglasKGlover <douglas.glover@outlook.com>
2019-08-19 23:52:49 -0400

    added svg arrows

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -5,11 +5,21 @@
                 <nuxt-link to="/" exact>Home</nuxt-link>
             </li>
             <li>
-                <nuxt-link to="/projects">&#10634; Projects</nuxt-link>
+                <nuxt-link to="/projects"><Arrow /> Projects</nuxt-link>
             </li>
             <li>
-                <nuxt-link to="/blog">&#10634; Blog</nuxt-link>
+                <nuxt-link to="/blog"><Arrow /> Blog</nuxt-link>
             </li>
         </ul>
     </nav>
 </template>
+
+<script>
+import Arrow from '~/assets/img/arrow.svg'
+
+export default {
+  components: {
+    Arrow
+  }
+}
+</script>
