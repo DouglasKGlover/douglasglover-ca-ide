9bf0b8179aa086536c71e2afcb69374690d1da96
DouglasKGlover <douglas.glover@outlook.com>
2019-08-20 16:13:55 -0400

    added anchors to the subnav

--- components/Subheader.vue
+++ components/Subheader.vue
@@ -1,5 +1,32 @@
 <template>
-    <aside id="subheader">
-        douglasglover.ca {{$nuxt.$route.path.split('/').join(' > ')}}
-    </aside>
+  <aside id="subheader">
+    <nuxt-link to="/" exact>douglasglover.ca</nuxt-link>&nbsp;<span v-html="WaterfallNav"></span>
+    <!-- <p v-for="(item, index) in $theFall" :key="index">
+      {{item}}
+    </p> -->
+  </aside>
 </template>
+
+<script>
+export default {
+  computed: {
+    WaterfallNav () {
+      const waterfall = this.$route.fullPath.split('/')
+      let output = ''
+      let currentBase = '/'
+      for (const i in waterfall) {
+        if (waterfall[i] !== '') {
+          if (i === waterfall.length) {
+            console.log('got here')
+          }
+          console.log(currentBase + waterfall[i])
+
+          currentBase += waterfall[i] + '/'
+          output += ' > ' + '<a href="' + currentBase + '">' + waterfall[i] + '</a>'
+        }
+      }
+      return output
+    }
+  }
+}
+</script>
