0bb6b4be8c85450b230311af2154907365c5176f
DouglasKGlover <douglas.glover@outlook.com>
2019-08-20 20:50:24 -0400

    added some mobile styles for the grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,9 @@
 <template>
   <section class="content" contenteditable="true" spellcheck="false">
-    <ul v-if="projects">
-        <li v-for="(project, index) in projects" :key="index">
-          {{project.fields.title}}
-        </li>
-    </ul>
-    <ul v-if="blogs">
-        <li v-for="(blog, index) in blogs" :key="index">
-          {{blog.fields.title}}
-        </li>
-    </ul>
+    <h1>I'm Douglas Glover</h1>
+    <h3>A Toronto-based Freelance Web Developer</h3>
+    <p>I'm not an author or a cameraman or a style consultant (in fact I suggest against asking me for clothing suggestions)...</p>
+    <p>But I do sure like the internet and video games, and so I surround myself with 'em. In fact I love coding so much I've designed my site here based on the programs I use to code with as a way to share with you my view of the world. Go ahead, click over here and get typing.</p>
   </section>
 </template>
 
